1dfc8927b5b5b31652d6c52645e80825e52d8505
rokaya magdi <rokaiamagdi@gmail.com>
2021-03-29 15:18:07 +0200

    change the condition of shouldBeUpdated to allow spinner to be visable

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,7 +5,10 @@ import "./Modal.css";
 class Modal extends Component {
   state = {};
   shouldComponentUpdate = (nextProps, nextState) => {
-    return nextProps.show !== this.props.show;
+    return (
+      nextProps.show !== this.props.show ||
+      nextProps.children !== this.props.children
+    );
     // only update this componentt when an only  when props.show is changed
   };
 
